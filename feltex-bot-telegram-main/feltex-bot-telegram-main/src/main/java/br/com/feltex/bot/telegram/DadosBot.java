55b77051fef02307bcb09a1c087f0fcb89452994
clopesreg <clopesreg@gmail.com>
2022-05-20 23:46:50 -0300

    Primeiro commit BOT

---
+++ feltex-bot-telegram-main/feltex-bot-telegram-main/src/main/java/br/com/feltex/bot/telegram/DadosBot.java
@@ -0,0 +1,6 @@
+package br.com.feltex.bot.telegram;
+
+public class DadosBot {
+    public static final String BOT_TOKEN = "5160385590:AAGdnqFCxIsLQE79FFOCv8eTn59A5QPH6d8";
+    public static final String BOT_USER_NAME = "@Jarviss5_bot";
+}
